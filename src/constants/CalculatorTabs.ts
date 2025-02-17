7d5b9f7b109ac31479dbe5e541ee67ae21c65887
maga0721 <mahesh.garade@netcracker.com>
2025-02-17 14:14:15 +0530

    Changed tab sequence

--- src/constants/CalculatorTabs.ts
+++ src/constants/CalculatorTabs.ts
@@ -2,6 +2,6 @@ import GoldValuation from "../calculators/gold-valuation/GoldValuation";
 import LoanDurationCalculator from "../calculators/loan-duration-calculator/LoanDurationCalculator";
 
 export const calculatorTabs = [
-  { tabName: "Gold Valuation", component: GoldValuation },
   { tabName: "Loan Duration", component: LoanDurationCalculator },
+  { tabName: "Gold Valuation", component: GoldValuation },
 ];
