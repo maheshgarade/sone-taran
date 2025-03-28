b2cbf34262b4732932bd5d622fd88237cc90dff1
Mahesh Garade <maheshgarade@gmail.com>
2025-03-28 19:06:49 +0530

    Fixed layout

--- src/features/calculators/wholesale-value-calculator/WholesaleValueCalculator.tsx
+++ src/features/calculators/wholesale-value-calculator/WholesaleValueCalculator.tsx
@@ -93,110 +93,118 @@ const WholesaleValueCalculator: React.FC = () => {
   };
 
   return (
-    <Container maxWidth="md" sx={{ pl: 0, pr: 0 }}>
-      <Paper elevation={3} sx={{ padding: 4, marginTop: 1.8 }}>
-        <Typography variant="h4" align="center" sx={{ mb: 4 }}>
-          Wholesale Value
-        </Typography>
-        <Box
-          component="form"
-          onSubmit={formik.handleSubmit}
-          sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}
-        >
-          {/* Input Fields */}
-          <Box sx={{ display: 'flex', flexDirection: 'row', gap: 3 }}>
-            <Box sx={{ flexGrow: 1 }}>
-              <TextField
-                fullWidth
-                type="number"
-                id="weight"
-                name="weight"
-                label="Weight (grams)"
-                value={formik.values.weight}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={formik.touched.weight && Boolean(formik.errors.weight)}
-                helperText={formik.touched.weight && formik.errors.weight}
-              />
-            </Box>
-            <Box sx={{ flexGrow: 1 }}>
-              <TextField
-                fullWidth
-                type="number"
-                id="purity"
-                name="purity"
-                label="Purity (%)"
-                value={formik.values.purity}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={formik.touched.purity && Boolean(formik.errors.purity)}
-                helperText={formik.touched.purity && formik.errors.purity}
-              />
-              {formik.values.purity && (
-                <Typography
-                  sx={{ mt: 1 }}
-                  variant="body2"
-                  color="text.secondary"
-                >
-                  {`${(parseFloat(formik.values.purity) - 0.4).toFixed(
-                    1
-                  )}% + 0.4% = ${formik.values.purity}%`}
-                </Typography>
-              )}
-            </Box>
-          </Box>
-
-          <Box sx={{ display: 'flex', flexDirection: 'row', gap: 3 }}>
-            <TextField
-              fullWidth
-              type="number"
-              id="wastage"
-              name="wastage"
-              label="Wastage (%)"
-              value={formik.values.wastage}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.wastage && Boolean(formik.errors.wastage)}
-              helperText={formik.touched.wastage && formik.errors.wastage}
-            />
-            <TextField
-              fullWidth
-              type="number"
-              id="goldRate99_5"
-              name="goldRate99_5"
-              label="Gold Rate (₹/gram for 99.5%)"
-              value={formik.values.goldRate99_5}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              error={
-                formik.touched.goldRate99_5 &&
-                Boolean(formik.errors.goldRate99_5)
-              }
-              helperText={
-                formik.touched.goldRate99_5 && formik.errors.goldRate99_5
-              }
-            />
-          </Box>
-
-          {/* Buttons */}
+    <>
+      <Container maxWidth="sm" sx={{ pl: 0, pr: 0 }}>
+        <Paper elevation={3} sx={{ padding: 4, marginTop: 1.8 }}>
+          <Typography variant="h4" align="center" sx={{ mb: 4 }}>
+            Wholesale Value
+          </Typography>
           <Box
-            sx={{ display: 'flex', justifyContent: 'space-between', gap: 2 }}
+            component="form"
+            onSubmit={formik.handleSubmit}
+            sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}
           >
-            <Button fullWidth type="submit" variant="contained" color="primary">
-              Calculate
-            </Button>
-            <Button
-              fullWidth
-              type="button"
-              variant="outlined"
-              color="secondary"
-              onClick={handleReset}
-            >
-              Reset
-            </Button>
-          </Box>
-        </Box>
+            {/* Input Fields */}
+            <Box sx={{ display: 'flex', flexDirection: 'row', gap: 3 }}>
+              <Box sx={{ flexGrow: 1 }}>
+                <TextField
+                  fullWidth
+                  type="number"
+                  id="weight"
+                  name="weight"
+                  label="Weight (grams)"
+                  value={formik.values.weight}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  error={formik.touched.weight && Boolean(formik.errors.weight)}
+                  helperText={formik.touched.weight && formik.errors.weight}
+                />
+              </Box>
+              <Box sx={{ flexGrow: 1 }}>
+                <TextField
+                  fullWidth
+                  type="number"
+                  id="purity"
+                  name="purity"
+                  label="Purity (%)"
+                  value={formik.values.purity}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  error={formik.touched.purity && Boolean(formik.errors.purity)}
+                  helperText={formik.touched.purity && formik.errors.purity}
+                />
+                {formik.values.purity && (
+                  <Typography
+                    sx={{ mt: 1 }}
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    {`${(parseFloat(formik.values.purity) - 0.4).toFixed(
+                      1
+                    )}% + 0.4% = ${formik.values.purity}%`}
+                  </Typography>
+                )}
+              </Box>
+            </Box>
 
+            <Box sx={{ display: 'flex', flexDirection: 'row', gap: 3 }}>
+              <TextField
+                fullWidth
+                type="number"
+                id="wastage"
+                name="wastage"
+                label="Wastage (%)"
+                value={formik.values.wastage}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.wastage && Boolean(formik.errors.wastage)}
+                helperText={formik.touched.wastage && formik.errors.wastage}
+              />
+              <TextField
+                fullWidth
+                type="number"
+                id="goldRate99_5"
+                name="goldRate99_5"
+                label="Gold Rate (₹/gram for 99.5%)"
+                value={formik.values.goldRate99_5}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.goldRate99_5 &&
+                  Boolean(formik.errors.goldRate99_5)
+                }
+                helperText={
+                  formik.touched.goldRate99_5 && formik.errors.goldRate99_5
+                }
+              />
+            </Box>
+
+            {/* Buttons */}
+            <Box
+              sx={{ display: 'flex', justifyContent: 'space-between', gap: 2 }}
+            >
+              <Button
+                fullWidth
+                type="submit"
+                variant="contained"
+                color="primary"
+              >
+                Calculate
+              </Button>
+              <Button
+                fullWidth
+                type="button"
+                variant="outlined"
+                color="secondary"
+                onClick={handleReset}
+              >
+                Reset
+              </Button>
+            </Box>
+          </Box>
+        </Paper>
+      </Container>
+      <Box sx={{ maxWidth: '80%', margin: 'auto' }}>
         {/* Results Section */}
         {calculated && calculatedValues && (
           <Box sx={{ marginTop: '1rem' }}>
@@ -211,40 +219,10 @@ const WholesaleValueCalculator: React.FC = () => {
               <WastageDetailsCard />
               <ItemDetailsCard />
             </Box>
-
-            <Box
-              sx={{
-                mt: 3,
-                p: 2,
-                border: '1px solid #ccc',
-                borderRadius: '4px',
-                backgroundColor: '#f9f9f9',
-              }}
-            >
-              <Typography variant="h6">Results:</Typography>
-              <Typography>
-                Item wt: {calculatedValues.itemWeight.toFixed(3)} gms
-              </Typography>
-              <Typography>Total Payment in Gold:</Typography>
-              <Typography>
-                99.9%: {calculatedValues.netPureGold99_9.toFixed(3)} gms
-              </Typography>
-              <Typography>
-                99.5%: {calculatedValues.netPureGold99_5.toFixed(3)} gms
-              </Typography>
-              <Typography>
-                Wastage Weight (99.5%):{' '}
-                {calculatedValues.wastageWeight.toFixed(2)} gms
-              </Typography>
-              <Typography>
-                Cash Payment for 99.5%: ₹
-                {calculatedValues.cashEquivalent99_5.toFixed(2)}
-              </Typography>
-            </Box>
           </Box>
         )}
-      </Paper>
-    </Container>
+      </Box>
+    </>
   );
 };
 
